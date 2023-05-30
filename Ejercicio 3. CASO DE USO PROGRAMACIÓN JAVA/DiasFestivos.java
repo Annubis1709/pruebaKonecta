e7be323f48409d014fa3f2b48b4f1715ce6c0aa9
Edier Sanchez Sanchez <sansankind@gmail.com>
2023-05-30 18:55:05 -0500

    Se implementó try-with-resources, para facilitar la gestión de recursos y garantizar que se cierren automáticamente

--- Ejercicio 3. CASO DE USO PROGRAMACIÓN JAVA/DiasFestivos.java
+++ Ejercicio 3. CASO DE USO PROGRAMACIÓN JAVA/DiasFestivos.java
@@ -1,17 +1,19 @@
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DiasFestivos {
     public List<LocalDate> cargarDiasFestivos(String archivo) {
-        try {
-            return Files.lines(Paths.get(archivo))
-                    .map(LocalDate::parse)
+
+        try (Stream<String> lines = Files.lines(Path.of(archivo)) ) {
+            return lines.map(LocalDate::parse)
                     .collect(Collectors.toList());
-        } catch (Exception exception) {
+
+        } catch (IOException exception) {
             System.out.println("Error al leer el archivo de dias festivos.");
             return List.of();
         }
